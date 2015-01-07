30ef5927e6ceb79a436046c0db07a1c27fdaf68e
qurihara <qurihara@gmail.com>
2015-01-07 21:12:50 +0900

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace WiiFitViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナ変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナで生成されたコード
+
+        /// <summary>
+        /// デザイナ サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディタで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(29, 38);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 240);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(716, 380);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(714, 418);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Disconnect";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(725, 255);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(725, 280);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(725, 295);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(803, 455);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,81 @@
+﻿// Codes by http://www.kosaka-lab.com/tips/2009/02/wiiwii-fit.php
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using WiimoteLib;    //WimoteLibの宣言
+
+namespace WiiFitViewer
+{
+    public partial class Form1 : Form
+    {
+        Wiimote wm = new Wiimote();
+
+        public Form1()
+        {
+            InitializeComponent();
+            //他スレッドからのコントロール呼び出し許可
+            Control.CheckForIllegalCrossThreadCalls = false;  
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Wiimoteの接続
+            this.wm.Connect();
+
+            //イベント関数の登録
+            this.wm.WiimoteChanged += wm_WiimoteChanged;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.wm.Disconnect();//Wiiリモコンを切断
+        }
+
+        void wm_WiimoteChanged(object sender, WiimoteChangedEventArgs args)
+        {
+            //WiimoteStateの値を取得
+            WiimoteState ws = args.WiimoteState;
+
+            //ピクチャーボックスへ描画
+            this.DrawForms(ws);
+
+            //ラベル
+
+            //重さ(Kg)表示
+            this.label1.Text = ws.BalanceBoardState.WeightKg + "kg";
+            //重心のX座標表示
+            this.label2.Text = "X:" +
+                ws.BalanceBoardState.CenterOfGravity.X;
+            //重心のY座標表示
+            this.label3.Text = "Y:" +
+                ws.BalanceBoardState.CenterOfGravity.Y;
+        }
+
+        //フォーム描写関数
+        public void DrawForms(WiimoteState ws)
+        {
+            //pictureBox1のグラフィックスを取得
+            Graphics g = this.pictureBox1.CreateGraphics();
+            g.Clear(Color.Black);     //画面を黒色にクリア
+
+            //X、Y座標の計算
+            float x =
+                (wm.WiimoteState.BalanceBoardState.CenterOfGravity.X
+                + 20.0f) * 10;    //表示位置(X座標)を求める
+            float y =
+                (wm.WiimoteState.BalanceBoardState.CenterOfGravity.Y
+                + 12.0f) * 10;    //表示位置(Y座標)を求める
+
+            //赤色でマーカを描写
+            g.FillEllipse(Brushes.Red, x, y, 10, 10);
+
+            g.Dispose();  //グラフィックスを開放
+        }
+    }
+}
